c29b0a3e43a3e7a56e3b6bdfc12e7b6ed8c76cb6
Horia Toma <toma.horia@gmail.com>
2018-09-27 14:09:47 +0200

    Added example of R script for charting to be used in Azure ML Studio

---
+++ PocAnomaly/RScript.R
@@ -0,0 +1,31 @@
+﻿# to be added to an R Script block in ML Studio
+
+library("magrittr")
+library("ggplot2")
+
+# load data from input
+taxis <- maml.mapInputPort(1)
+
+# build a chart
+# here plotting longitude vs latitude using points
+# and setting the limits on the viewport, if needed
+p1 <- taxis %>%
+        ggplot(aes(pickup_longitude, pickup_latitude, color = passenger_count)) +
+        geom_point() +
+        xlim(-75, -72) +
+        ylim(40, 42)
+        
+# exporting the chart to an in-memory pdf        
+pdf('mypdf.pdf', width = 6, height = 4, paper = 'special')
+plot(p1)  
+dev.off()
+
+# using a base64 encoding in order to build the data frame to be exported
+library(caTools)  
+b64ePDF <- function(filename) {  
+            maxFileSizeInBytes <- 15 * 1024 * 1024 # 5 MB  
+            return(base64encode(readBin(filename, "raw", n = maxFileSizeInBytes)))  
+}  
+
+d2 <- data.frame(pdf = b64ePDF("mypdf.pdf"))
+maml.mapOutputPort("d2");
